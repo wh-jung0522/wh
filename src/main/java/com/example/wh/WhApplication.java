65dc75c346a6419a089d33cd1cc16a97d3f8b30a
whjung <kwdg1213@com2us.com>
2022-04-08 18:06:07 +0900

    Initial commit

---
+++ src/main/java/com/example/wh/WhApplication.java
@@ -0,0 +1,13 @@
+package com.example.wh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wh/WhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
