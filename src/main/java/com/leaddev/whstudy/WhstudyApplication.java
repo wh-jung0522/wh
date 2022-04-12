75dcca2a1cd46fb6b30346919af4abd1f57f4a2b
whjung <kwdg1213@com2us.com>
2022-04-12 17:53:33 +0900

    [Remove/Add] Static Test Page / JSP page Test

--- src/main/java/com/leaddev/whstudy/WhstudyApplication.java
+++ src/main/java/com/leaddev/whstudy/WhstudyApplication.java
@@ -1,11 +1,12 @@
 package com.leaddev.whstudy;
 
 import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"controller"})
+@ComponentScan
 public class WhstudyApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/leaddev/whstudy/controller/WebController.java
+++ src/main/java/com/leaddev/whstudy/controller/WebController.java
@@ -2,14 +2,22 @@ package com.leaddev.whstudy.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+//import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebController {
 
-    @RequestMapping(value = "/", method= RequestMethod.GET)
-    public String home(){
-        return "index";
+
+    @RequestMapping(value = "/")
+    public @ResponseBody String root() throws Exception{
+        return "JSP Test";
+    }
+    @RequestMapping(value = "/hello")
+    public String index() throws Exception{
+        return "hello";
     }
 
 }
